20aca9454b1534939bb59016da420aaa52a65824
Karthik <karthik12.xworkz@gmail.com>
2024-06-24 21:15:28 +0530

    this is my first commit

---
+++ Application.java
@@ -0,0 +1,12 @@
+public class Application{
+static String application_year = "24-25";
+static String reg_num = "435678";
+static String gender = "Male";
+static int age = 15;
+static String fathername = "Rajesh";
+static String mothername = "Rani";
+static String occupation = "Farmer";
+static int standard = 10;
+static String cast = "General";
+
+}
