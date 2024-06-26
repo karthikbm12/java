fe61ffc343a8dc2d03d6049750da815a0285bdae
karthik12 <karthik12>
2024-06-26 23:00:51 +0530

    tuesday task

---
+++ Multiples.java
@@ -0,0 +1,10 @@
+public class Multiples{
+	public static void main(String args[]){
+	int[]numbers= {1,2,3,4,5,6,7,8,9,10,12,14,16};
+	for(int i=0;i<numbers.length;i++){
+		if(numbers[i]%3==0){
+	System.out.println("The mulitiples of three are :"+numbers[i]);
+	}
+	}
+	}
+}
