92ffdd35b2ce50a944872bb7ad3e16205883d552
karthik12 <karthik12>
2024-07-01 22:18:17 +0530

    28-06-24

---
+++ Airpodes.java
@@ -0,0 +1,14 @@
+public class Airpodes{
+	
+	
+	public static void company(){
+		System.out.println("fireboult");
+		
+	}
+	
+	public static void year (){
+		System.out.println("year is 2015");
+	
+	}
+
+}
