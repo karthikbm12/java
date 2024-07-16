1796f71181ea34f43b8894936c875d3557aa6bd6
karthik12 <karthik12xworkz@gmail.com>
2024-07-16 20:53:27 +0530

    15-07-24

---
+++ snippet/Loan.java
@@ -0,0 +1,25 @@
+
+public class Loan{
+
+public static String person_name = "raju";
+public static byte loan_id = 24;
+public static short age = 45;
+public static double points = 400.0;
+public static int loan_no = 45621;
+public static long amount = 7500000L;
+public static boolean success = true;
+public static String Bank_name = "Icici";
+public static float rating = 9.5f;
+public static char gender = 'M';
+public static int salary = 20000;
+public static String job = "software";
+public static byte date = 25;
+public static Boolean Sanction = false;
+public static short weight = 40;
+public static double height = 5.0;
+public static String location = "Bangalore";
+public Loan(){
+	
+	System.out.println("The Above information shows Loan details");
+}
+}
---
+++ snippet/LoanRunner.java
@@ -0,0 +1,88 @@
+
+public class LoanRunner{
+
+public static void main(String args[]){
+
+Loan data = new Loan();
+System.out.println("The loan person name is:"+data.person_name);
+System.out.println("The loan person name is:"+data.loan_id);
+System.out.println("The loan person name is:"+data.age);
+System.out.println("The loan person name is:"+data.points);
+System.out.println("The loan person name is:"+data.loan_no);
+System.out.println("The loan person name is:"+data.amount);
+System.out.println("The loan person name is:"+data.success);
+System.out.println("The loan person name is:"+data.Bank_name);
+System.out.println("The loan person name is:"+data.rating);
+System.out.println("The loan person name is:"+data.person_name);
+System.out.println("The loan person name is:"+data.gender);
+System.out.println("The loan person name is:"+data.salary);
+System.out.println("The loan person name is:"+data.job);
+System.out.println("The loan person name is:"+data.date);
+System.out.println("The loan person name is:"+data.Sanction);
+System.out.println("The loan person name is:"+data.height);
+System.out.println("The loan person name is:"+data.weight);
+System.out.println("The loan person name is:"+data.location);
+
+Loan data1 = new Loan();
+System.out.println("The loan person name is:"+data1.person_name);
+System.out.println("The loan person name is:"+data1.loan_id);
+System.out.println("The loan person name is:"+data1.age);
+System.out.println("The loan person name is:"+data1.points);
+System.out.println("The loan person name is:"+data1.loan_no);
+System.out.println("The loan person name is:"+data1.amount);
+System.out.println("The loan person name is:"+data1.success);
+System.out.println("The loan person name is:"+data1.Bank_name);
+System.out.println("The loan person name is:"+data1.rating);
+System.out.println("The loan person name is:"+data1.person_name);
+System.out.println("The loan person name is:"+data1.gender);
+System.out.println("The loan person name is:"+data1.salary);
+System.out.println("The loan person name is:"+data1.job);
+System.out.println("The loan person name is:"+data1.date);
+System.out.println("The loan person name is:"+data1.Sanction);
+System.out.println("The loan person name is:"+data1.height);
+System.out.println("The loan person name is:"+data1.weight);
+System.out.println("The loan person name is:"+data1.location);
+
+Loan data2 = new Loan();
+System.out.println("The loan person name is:"+data2.person_name);
+System.out.println("The loan person name is:"+data2.loan_id);
+System.out.println("The loan person name is:"+data2.age);
+System.out.println("The loan person name is:"+data2.points);
+System.out.println("The loan person name is:"+data2.loan_no);
+System.out.println("The loan person name is:"+data2.amount);
+System.out.println("The loan person name is:"+data2.success);
+System.out.println("The loan person name is:"+data2.Bank_name);
+System.out.println("The loan person name is:"+data2.rating);
+System.out.println("The loan person name is:"+data2.person_name);
+System.out.println("The loan person name is:"+data2.gender);
+System.out.println("The loan person name is:"+data2.salary);
+System.out.println("The loan person name is:"+data2.job);
+System.out.println("The loan person name is:"+data2.date);
+System.out.println("The loan person name is:"+data2.Sanction);
+System.out.println("The loan person name is:"+data2.height);
+System.out.println("The loan person name is:"+data2.weight);
+System.out.println("The loan person name is:"+data2.location);
+Loan data3 = new Loan();
+System.out.println("The loan person name is:"+data3.person_name);
+System.out.println("The loan person name is:"+data3.loan_id);
+System.out.println("The loan person name is:"+data3.age);
+System.out.println("The loan person name is:"+data3.points);
+System.out.println("The loan person name is:"+data3.loan_no);
+System.out.println("The loan person name is:"+data3.amount);
+System.out.println("The loan person name is:"+data3.success);
+System.out.println("The loan person name is:"+data3.Bank_name);
+System.out.println("The loan person name is:"+data3.rating);
+System.out.println("The loan person name is:"+data3.person_name);
+System.out.println("The loan person name is:"+data3.gender);
+System.out.println("The loan person name is:"+data3.salary);
+System.out.println("The loan person name is:"+data3.job);
+System.out.println("The loan person name is:"+data3.date);
+System.out.println("The loan person name is:"+data3.Sanction);
+System.out.println("The loan person name is:"+data3.height);
+System.out.println("The loan person name is:"+data3.weight);
+System.out.println("The loan person name is:"+data3.location);
+}
+
+
+
+}
