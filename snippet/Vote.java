805f80e7f62c9d01daa7a908bb8b8cc6a4dbe22d
karthik12 <karthik12xworkz@gmail.com>
2024-07-21 23:21:43 +0530

    17-07-24

---
+++ snippet/Vote.java
@@ -0,0 +1,10 @@
+public class Vote{
+	public static void main(String args[]){
+	int age = 9;
+	if(age<18){
+	System.out.println("Not elegible to vote"+age);
+	}else{
+		System.out.println("elegible to vote"+age);
+	}
+	}
+	}
